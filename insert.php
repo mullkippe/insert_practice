0689950c0d5038eca0733600ee35d81c2ff56b1f
AYUMI WADA <65645313+mullkippe@users.noreply.github.com>
2021-03-27 02:21:35 +0900

    登録画面の練習

---
+++ insert.php
@@ -0,0 +1,46 @@
+<?php
+//1. POSTデータ取得
+
+//まず前のphpからデーターを受け取る（この受け取ったデータをもとにbindValueと結びつけるため）
+$name = $_POST["name"];
+$email = $_POST["email"];
+$naiyou = $_POST["naiyou"];
+//上記で受け取ったデータをdbに登録していく処理を下で行う
+
+//2. DB接続します xxxにDB名を入力する
+//ここから作成したDBに接続をしてデータを登録します xxxxに作成したデータベース名を書きます
+// mamppの方は
+// $pdo = new PDO('mysql:dbname=xxx;charset=utf8;host=localhost', 'root', 'root');
+
+try {
+    $pdo = new PDO('mysql:dbname=a_db;charset=utf8;host=localhost','root', 'root');
+} catch (PDOException $e) {
+    exit('DbConnectError:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成 //ここにカラム名を入力する
+//xxx_table(テーブル名)はテーブル名を入力します
+//sqlという変数に「:***」という代わりの変数を入れる(直接変数は入れられない)入れたものを$sqlに預けた / その後に$pdoの中のprepareという関数に＄sqlを渡したことで、$stmtで一回保持している。
+$sql = "INSERT INTO a_table(id, name, email, naiyou,
+indate )VALUES(NULL, :name, :email, :naiyou, sysdate())";
+
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':naiyou', $naiyou, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if ($status==false) {
+    //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+    $error = $stmt->errorInfo();
+    exit("QueryError:".$error[2]);
+} else {
+    //５．index.phpへリダイレクト 書くときにLocation: in この:　のあとは半角スペースがいるので注意！！処理が終わった後にindex.phpに飛ぶ
+    header("Location: index.php");
+    exit;
+}
+
+?>
